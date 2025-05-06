f9f5ed75883adc20d643e066d66e7fe5f7bde6c1
Jayavel <jayavel1520@gmail.com>
2025-05-06 10:52:23 +0530

    Day 02

--- Day02/Main.c
+++ Day02/Main.c
@@ -16,9 +16,20 @@ void main()
     // char character = integer; // This line implicity converts the integer to character
     // printf("%c", character);
 
-    for (char character = 'A'; character <= 'Z'; character++)
-    {
-        printf("%c = %d\n", character, character);
-    }
+    // for (char character = 'A'; character <= 'Z'; character++)
+    // {
+    //     printf("%c = %d\n", character, character);
+    // }
+
+    // bmi calculation
+    // formula is w/m^2
+    float weight , meter, bmi;
+    printf("Enter the weight: ");
+    scanf("%f", &weight);
+    printf("Enter the meters: ");
+    scanf("%f", &meter);
+    meter = meter / 100;
+    bmi = weight / (meter * meter);
+    printf("%.2f", bmi);
     
 }
